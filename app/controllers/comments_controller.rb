f74d7c5f7b367231ead59eac7642bd3225625d7f
Carmina <cvdsoto@gmail.com>
2020-04-18 15:38:31 +1000

    comments can be added

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    comment = Comment.create comment_params
+    redirect_to comments_path
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:name, :comment)
+  end
+end
