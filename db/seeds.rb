91edd20952f89b64a4a4bc7d855838e9acc9de22
Zara <zara@okaayfinee.home>
2020-04-19 01:43:42 +1000

    Added viv's comments to seed, gradient button, centered stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,5 @@ Comment.destroy_all
 
 Comment.create :name => 'Phantom', :comment => "I am your angel of music!"
 Comment.create :name => 'Christine Daae', :comment => "Think of me fondly when we say goodbye"
+Comment.create :name => 'Viv Wan', :comment => 'Joel is awesome because he taught us how to not give a fuck about anything "non-essential" and "JUST GET ON WITH IT!"'
+Comment.create :name => 'Viv Wan', :comment => 'Joel is awesome because he switches off the light for us each morning as soon as he enters the classroom.'
