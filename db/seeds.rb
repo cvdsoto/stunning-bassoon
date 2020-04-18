4eae03a044c1941b78ad7712a8ecf16a1c7e027a
Carmina <cvdsoto@gmail.com>
2020-04-18 16:58:27 +1000

    added comments and fixed indentations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# LOL sorry I was watching Phantom of the Opera while I was doing this. Feel free to change these seed file when the time comes :)
 Comment.destroy_all
 
 Comment.create :name => 'Phantom', :comment => "I am your angel of music!"
