3208e40b80fcc2a132f0c47645de411f94ffc11e
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-04-23 07:59:53 +1000

    Added butterflies cuuuute

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+  // moving butterfly
+  const timerID = setInterval(function() {
+    $('#butterfly_1').animate({'left':'+=10px', 'bottom':'+=10px'}, "fast");
+  }, 30);
+
+  let count = 1;
+
+  setInterval(function() {
+    count += 1;
+    let left = (Math.random() * 2000) - 1000;
+    $butterfly = $(`<img src="/assets/butterfly.gif" id="butterfly_${count}" class="butterfly-fly"/>`);
+    $butterfly.css({
+      'bottom': '-200px',
+      'left': `${left}px`
+    });
+    $('#butterfly-cage').append($butterfly);
+    const timerID = setInterval(function() {
+      $(`#butterfly_${count}`).animate({'left':'+=10px', 'bottom':'+=10px'}, "fast");
+    }, 30);
+  }, 2000);
+
+
+  // bill murray image pop ups
   setInterval(function() {
     console.log("yes im here");
     const top = Math.random() * 50;
