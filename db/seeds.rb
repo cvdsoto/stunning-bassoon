eb3628adc4fc70b70441ea7a41959d9ee26513d0
Carmina <cvdsoto@gmail.com>
2020-04-23 11:08:52 +1000

    revised buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 # LOL sorry I was watching Phantom of the Opera while I was doing this. Feel free to change these seed file when the time comes :)
 Comment.destroy_all
 
-Comment.create :name => 'Phantom', :comment => "I am your angel of music!"
-Comment.create :name => 'Christine Daae', :comment => "Think of me fondly when we say goodbye"
 Comment.create :name => 'Viv Wan', :comment => 'Joel is awesome because he taught us how to not give a fuck about anything "non-essential" and "JUST GET ON WITH IT!"'
 Comment.create :name => 'Viv Wan', :comment => 'Joel is awesome because he switches off the light for us each morning as soon as he enters the classroom.'
 Comment.create :name => 'Zara', :comment => "Joel isn't a jerk :)"
